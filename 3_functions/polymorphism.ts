c38f540f6ae9e5a3ccb9d419b49403f63a971f03
NG <guraband@naver.com>
2023-04-12 00:07:36 +0900

    #3.3 Generic Recap 완료

--- 3_functions/polymorphism.ts
+++ 3_functions/polymorphism.ts
@@ -1,11 +1,10 @@
-type GenericPrint = {
-    <T>(arr: T[]): void
+type GenericPrint = <T, V>(arr: T[], b: V) => void
+
+
+const genericPrint: GenericPrint = (arr, b) => {
+    arr.forEach(i => console.log(i, b))
 }
 
-const genericPrint: GenericPrint = (arr) => {
-    arr.forEach(i => console.log(i))
-}
-
-genericPrint([1, 2, 3])
-genericPrint([true, false, true])
-genericPrint([1, true, "hi"])
+genericPrint([1, 2, 3], "hi")
+genericPrint([true, false, true], 7)
+genericPrint([1, true, "hi"], true)
