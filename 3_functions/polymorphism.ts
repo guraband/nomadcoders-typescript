59abccc01a91cf4993e0d3d7fd8470a16669cab6
NG <guraband@naver.com>
2023-04-12 22:58:17 +0900

    #3.4 Conclusions 완료

--- 3_functions/polymorphism.ts
+++ 3_functions/polymorphism.ts
@@ -8,3 +8,20 @@ const genericPrint: GenericPrint = (arr, b) => {
 genericPrint([1, 2, 3], "hi")
 genericPrint([true, false, true], 7)
 genericPrint([1, true, "hi"], true)
+
+type Player<E> = {
+    name: string
+    extraInfo: E
+}
+
+type SubPlayer = Player<{ age: number }>
+
+const player1: SubPlayer = {
+    name: 'kim',
+    extraInfo: { age: 20 }
+}
+
+const player2: Player<null> = {
+    name: 'lee',
+    extraInfo: null
+}
